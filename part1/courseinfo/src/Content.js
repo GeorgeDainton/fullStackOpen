b3ee44ab1e20ef5bb18bd7b48531d572a10a920b
George Dainton <gcdainton94@gmail.com>
2022-08-17 22:19:49 +0100

    Content refactoring

--- part1/courseinfo/src/Content.js
+++ part1/courseinfo/src/Content.js
@@ -1,18 +1,21 @@
-const Part = (props) => {
+const Part = ({name, exercises}) => {
+  console.log('Part')
   return (
     <div>
-      <p>{props.name} {props.exercises}</p>
+      <p>{name} {exercises}</p>
     </div>
   )
 }
 
 
-const Content = (props) => {
+const Content = ({parts}) => {
+  console.log('Content')
+  console.log({parts})
   return (
     <div>
-      <Part name={props.parts[0].name} exercises={props.parts[0].exercises} />
-      <Part name={props.parts[1].name} exercises={props.parts[1].exercises} />
-      <Part name={props.parts[2].name} exercises={props.parts[2].exercises} />
+      <Part name={parts[0].name} exercises={parts[0].exercises} />
+      <Part name={parts[1].name} exercises={parts[1].exercises} />
+      <Part name={parts[2].name} exercises={parts[2].exercises} />
   </div>
   )
 }
