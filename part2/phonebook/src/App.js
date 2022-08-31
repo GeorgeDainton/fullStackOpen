fb5098a07788f9353e1125e6d209d170ba8d806d
George Dainton <gcdainton94@gmail.com>
2022-08-31 18:35:53 +0100

    Answers saved to branch

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -1,28 +1,43 @@
 import { useState } from 'react'
+import Number from './components/Number'
 
-const App = () => {
-  const [persons, setPersons] = useState([
-    { name: 'Arto Hellas' }
-  ])
+const App = (props) => {
+  const [persons, setPersons] = useState(props.persons)
   const [newName, setNewName] = useState('')
+
+  const addPerson = (event) => {
+    event.preventDefault()
+    const personObject = {
+      name: newName,
+    }
+
+    setPersons(persons.concat(personObject))
+    setNewName('')
+  }
+
+  const handleNameChange = (event) => {
+    setNewName(event.target.value)
+  }
 
   return (
     <div>
       <h2>Phonebook</h2>
-      <form>
+      <form onSubmit={addPerson}>
         <div>
-          name: <input />
+          Name: <input onChange={handleNameChange} placeholder="..." />
         </div>
         <div>
           <button type="submit">Add</button>
         </div>
       </form>
+      <table>
       <h2>Numbers</h2>
+        {persons.map(person =>
+          <Number key={person.id} person={person}/>
+        )}
+      </table>
     </div>
   )
-
-
-
 }
 
 export default App;
--- part2/phonebook/src/index.js
+++ part2/phonebook/src/index.js
@@ -3,5 +3,12 @@ import ReactDOM from 'react-dom/client';
 
 import App from './App';
 
+const persons = [
+  { 
+    id: 1,
+    name: 'Arto Hellas', 
+  }
+]
 
-ReactDOM.createRoot(document.getElementById('root')).render(< App />)
+
+ReactDOM.createRoot(document.getElementById('root')).render(< App persons={persons}/>)
