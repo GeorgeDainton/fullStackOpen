d84857d9a6e87ffcc40d6bae97b4e3d86becc3e2
George Dainton <gcdainton94@gmail.com>
2022-09-05 20:50:11 +0100

    2.13 Complete

--- part2/countries/src/components/Country.js
+++ part2/countries/src/components/Country.js
@@ -1,6 +1,8 @@
 import React from 'react'
+import { useState } from 'react'
 
 const Country = ( {country, countryFilter, filteredCountries} ) => {
+  const [showView, setShowView] = useState(false)
   
   if(filteredCountries.length === 1) {
     return (
@@ -11,23 +13,37 @@ const Country = ( {country, countryFilter, filteredCountries} ) => {
         <b>Languages:</b>
         <ul>
           {Object.entries(country.languages).map(([key, value], i) => (
-            <li key={country.name.common}>{value}</li>
+            <li key={i}>{value}</li>
           ))}
         </ul>
-       
-        <img src={country.flags.svg} alt='flag'/>
+        <img src={country.flags.png} alt='flag'/>
       </>
-    
-        
     )
   } else if(countryFilter.length !== 0 && filteredCountries.length > 10) {
     return (
       <p>{'Too many matches, be more specific'}</p>
     )
-  } else if(countryFilter.length !== 0) {
+  } else if(countryFilter.length !== 0 && showView === false) {
       return (
-        <p>{country.name.common}</p>
+        <>
+          <p>{country.name.common}<button onClick={() => setShowView(true)}>Show</button></p>
+        </>
     )
+  } else if(countryFilter.length !== 0 && showView === true) {
+      return (
+          <>
+            <h1>{country.name.common}<button onClick={() => setShowView(false)}>Hide</button></h1>
+            <p>Capital {country.capital}</p>
+            <p>Area {country.area}</p>
+            <b>Languages:</b>
+            <ul>
+              {Object.entries(country.languages).map(([key, value], i) => (
+                <li key={i}>{value}</li>
+              ))}
+            </ul>
+            <img src={country.flags.png} alt='flag'/>
+          </>
+        )
   }}
 
 export default Country;
